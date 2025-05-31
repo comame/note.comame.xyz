c7253ea972f749d1d5e250af1b2245201c447ed5
comame <dev@comame.xyz>
2025-05-31 18:55:25 +0900

    エディタの見た目

--- front/src/components/editor.tsx
+++ front/src/components/editor.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { parse } from "../lib/markdown";
 import "./editor.css";
 import type { permission } from "../lib/types";
+import BracketButton from "./bracket_button";
 
 interface props {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
@@ -15,7 +16,6 @@ interface props {
 }
 
 export default function Editor({ onSubmit, editPost, demoMode }: props) {
-  const [text, setText] = useState("");
   const [markdown, setMarkdown] = useState("");
 
   const [tab, setTab] = useState<"editor" | "preview">("editor");
@@ -50,18 +50,12 @@ export default function Editor({ onSubmit, editPost, demoMode }: props) {
       }}
     >
       <nav id="editor-tab">
-        <ul>
-          <li>
-            <a href="#" id="tab-editor" onClick={() => setTab("editor")}>
-              Editor
-            </a>
-          </li>
-          <li>
-            <a href="#" id="tab-preview" onClick={() => setTab("preview")}>
-              Preview
-            </a>
-          </li>
-        </ul>
+        <BracketButton
+          values={[
+            { label: "Editor", onClick: () => setTab("editor") },
+            { label: "Preview", onClick: () => setTab("preview") },
+          ]}
+        />
       </nav>
 
       <div id="editor-main" className={tab === "editor" ? "" : "hide-touch"}>
@@ -77,11 +71,11 @@ export default function Editor({ onSubmit, editPost, demoMode }: props) {
           id="input"
           name="input"
           placeholder="本文"
-          value={text}
+          // value={text}
           defaultValue={editPost?.text ?? undefined}
           onChange={async (e) => {
             const md = e.currentTarget.value;
-            setText(md);
+            // setText(md);
             const parsed = await parse(md);
             setMarkdown(parsed);
           }}
